b9b986ff6fcee0b2c89900e9bb3e68d60fb0630f
JD Piquard <jdpiquard@portr.com>
2015-10-13 22:25:46 +0100

    At chapter 12.1

---
+++ leaderboard.js
@@ -0,0 +1,114 @@
+
+
+PlayersList = new Mongo.Collection('players');
+// store the document into a variable, similarly to an array/df
+
+if(Meteor.isClient){
+  Meteor.subscribe('thePlayers');  // see alo Meteor.publish for making data available to client
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      var currentUserID = Meteor.user();
+      return PlayersList.find({createdBy: currentUserID}, // technically no longer needed, since accounts already make sure this happens!
+        {sort: {score: -1, name: 1}}); // make available PlayersList to template
+      // the first curly braces are the first argument, saying to return all data
+      // 
+      // note that sort does not require $ as for $set and $inc operators
+    },
+
+    'stuff': function(){
+      return "just making sure this works";
+    },
+
+    'totalPlayers': function(){ // returns the number of players stored in the collection
+      return PlayersList.find().count();
+    },
+
+    'totalUserPlayers': function(){ // NOT WORKING - should return players created by user
+      return PlayersList.find({createdBy: currentUserID}).count();
+    },
+
+    'selectedClass': function(){  // applies yellow background for selected items
+      // compares current selectedID with PlayerID to determine whether to apply a special class
+
+      var playerID = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerID == selectedPlayer){
+        return "selected";
+      }
+    },
+
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerID = this._id;  // 'this' works by selecting the last DOM touched
+        // interestingly, this would return any other property saved into Mongo, 
+        // since Meteor effectively attaches all these properties here. e.g. this.score works
+      Session.set('selectedPlayer', playerID); // save playerID into the current session cache
+        // it's an useful way of keeping track of what has been selected
+      
+    },
+
+    'click .increment': function(){ 
+      // similar to jQuery's $(.increment).click(function(){...})
+
+      var selectedPlayer = Session.get('selectedPlayer');
+      // PlayersList.update(selectedPlayer, {$set: {score: 10}});
+      //   // this Mongo update function requires $set, otherwise it will delete the other fields
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+        // the $inc operator adds to the existing field
+    },
+
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+    },
+
+    'click .delete': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      var confirm = window.confirm("Are you sure you want to remove " + PlayersList.findOne(selectedPlayer).name + " ?");
+      if(confirm){
+        PlayersList.remove(selectedPlayer);
+      }
+    }
+
+
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+        // prevents default form behaviour, namely webpage refresh
+
+      var playerNameVar = event.target.newPlayerName.value;
+      var playerScore = parseInt(event.target.newScore.value); // needs conversion into an integer otherwise throws error
+        // obtains the value inside the form 
+
+      var currentUserID = Meteor.user();
+
+      PlayersList.insert({  // inserts field into Mongo Document
+        name: playerNameVar,
+        score: playerScore,
+        createdBy: currentUserID
+      });
+      event.target.newPlayerName.value = ""; // resets form to blank
+      event.target.newScore.value = "";
+    }
+  });
+
+}
+
+if(Meteor.isServer){
+  
+  Meteor.publish('thePlayers', function(){
+    var currentUserID = this.userID;
+    return PlayersList.find();
+  });
+
+}
+
