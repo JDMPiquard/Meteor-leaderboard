be102f5163f1c17e8b89f2073f9b27f572d1d758
JD Piquard <jdpiquard@portr.com>
2015-10-18 13:31:00 +0100

    moved database functions to server

--- leaderboard.js
+++ leaderboard.js
@@ -8,11 +8,10 @@ if(Meteor.isClient){
 
   Template.leaderboard.helpers({
     'player': function(){
-      var currentUserID = Meteor.user();
-      return PlayersList.find({createdBy: currentUserID}, // technically no longer needed, since accounts already make sure this happens!
-        {sort: {score: -1, name: 1}}); // make available PlayersList to template
+      //var currentUserID = Meteor.user();
+      return PlayersList.find({}, {sort: {score: -1, name: 1}}); // make available PlayersList to template
       // the first curly braces are the first argument, saying to return all data
-      // 
+      //
       // note that sort does not require $ as for $set and $inc operators
     },
 
@@ -22,10 +21,6 @@ if(Meteor.isClient){
 
     'totalPlayers': function(){ // returns the number of players stored in the collection
       return PlayersList.find().count();
-    },
-
-    'totalUserPlayers': function(){ // NOT WORKING - should return players created by user
-      return PlayersList.find({createdBy: currentUserID}).count();
     },
 
     'selectedClass': function(){  // applies yellow background for selected items
@@ -47,33 +42,36 @@ if(Meteor.isClient){
   Template.leaderboard.events({
     'click .player': function(){
       var playerID = this._id;  // 'this' works by selecting the last DOM touched
-        // interestingly, this would return any other property saved into Mongo, 
+        // interestingly, this would return any other property saved into Mongo,
         // since Meteor effectively attaches all these properties here. e.g. this.score works
       Session.set('selectedPlayer', playerID); // save playerID into the current session cache
         // it's an useful way of keeping track of what has been selected
-      
+
     },
 
-    'click .increment': function(){ 
+    'click .increment': function(){
       // similar to jQuery's $(.increment).click(function(){...})
 
       var selectedPlayer = Session.get('selectedPlayer');
       // PlayersList.update(selectedPlayer, {$set: {score: 10}});
       //   // this Mongo update function requires $set, otherwise it will delete the other fields
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+      // PlayersList.update(selectedPlayer, {$inc: {score: 5} });
         // the $inc operator adds to the existing field
+
+      Meteor.call('modPlayerScore', selectedPlayer, 5);
     },
 
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+      //PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+      Meteor.call('modPlayerScore', selectedPlayer, -5);
     },
 
     'click .delete': function(){
       var selectedPlayer = Session.get('selectedPlayer');
       var confirm = window.confirm("Are you sure you want to remove " + PlayersList.findOne(selectedPlayer).name + " ?");
       if(confirm){
-        PlayersList.remove(selectedPlayer);
+        Meteor.call('removePlayerData',selectedPlayer);
       }
     }
 
@@ -87,28 +85,54 @@ if(Meteor.isClient){
 
       var playerNameVar = event.target.newPlayerName.value;
       var playerScore = parseInt(event.target.newScore.value); // needs conversion into an integer otherwise throws error
-        // obtains the value inside the form 
+        // obtains the value inside the form
 
-      var currentUserID = Meteor.user();
+      Meteor.call('insertPlayerData', playerNameVar, playerScore);
 
-      PlayersList.insert({  // inserts field into Mongo Document
-        name: playerNameVar,
-        score: playerScore,
-        createdBy: currentUserID
-      });
+      // Resetting the form
       event.target.newPlayerName.value = ""; // resets form to blank
       event.target.newScore.value = "";
+
     }
   });
 
-}
+} // close isClient
 
 if(Meteor.isServer){
-  
+  // the following makes "PlayersList" collection available to the client
   Meteor.publish('thePlayers', function(){
-    var currentUserID = this.userID;
-    return PlayersList.find();
+    var currentUserID = this.userId;
+    return PlayersList.find({createdBy: currentUserID})
   });
 
-}
+  // methods
+  Meteor.methods({
+    'sendLogMessage': function(){
+      console.log(Meteor.user() + Meteor.userId())
+    },
 
+    'insertPlayerData': function(playerNameVar, playerScore){
+      if (playerNameVar) {  // only run if string is NOT empty
+        if (isNaN(playerScore)) {playerScore = 0}  // change score to 0 if no score was set
+        var currentUserID = Meteor.userId();
+        PlayersList.insert({
+          name: playerNameVar,
+          score: playerScore,
+          createdBy: currentUserID
+        })
+      }
+    },
+
+    'removePlayerData': function(selectedPlayer) {
+      var currentUserID = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserID});
+    },
+
+    'modPlayerScore': function(selectedPlayer, newScore) {
+      var currentUserID=Meteor.userId();
+      PlayersList.update({_id: selectedPlayer, createdBy: currentUserID}, {$inc: {score: newScore}});
+    }
+
+  })
+
+}
